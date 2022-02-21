ee4f8e1e8cde2c671901a5db168b850addb08ec4
Arashfa0301 <Arash.f.taleghani@gmail.com>
2022-02-21 15:56:25 +0100

    Add asteroids project files

---
+++ src/main/java/asteroids/AsteroidsApp.java
@@ -0,0 +1,23 @@
+package asteroids;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AsteroidsApp extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Asteroids");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("AsteroidsFx.fxml"))));
+        primaryStage.show();
+    }
+
+}
