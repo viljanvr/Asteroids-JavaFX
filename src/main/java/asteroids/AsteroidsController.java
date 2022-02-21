5a001dbe7de75475b287f117f598dabb18853d46
Viljan Gerbrand Emanuel Van Raaij <viljanvanraaij@gmail.com>
2022-02-21 22:39:50 +0100

    Add animation loop

--- src/main/java/asteroids/AsteroidsController.java
+++ src/main/java/asteroids/AsteroidsController.java
@@ -9,34 +9,39 @@ import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import java.time.Instant;
+
 
 public class AsteroidsController {
-    
+    public Timer timer = new Timer();
     
 
     @FXML public Canvas canvas = new Canvas(800,600);
 
     public GraphicsContext gc;
 
-    double x = 1.0;
+
     
     public void initialize(){
         gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.BLACK);
         gc.fillRect(0,0, 800, 600);
-        gc.drawImage(new Image("asteroids/spaceship.png"), 0.0, 0.0);
+        gc.drawImage(new Image("asteroids/spaceship.png"), 100.0, 100.0);
+        gc.drawImage(new Image("asteroids/asteroid.png"), 100.0, 200.0);
+        timer.start();
+
+
+        
     }
 
-    AnimationTimer loop = new AnimationTimer() {
+    private class Timer extends AnimationTimer{
+        private double x = 1.0;
+        @Override
         public void handle(long nanotime){
-            gc.drawImage(new Image("asteroids/spaceship.png"), 0.0, x);
-            this.x += 1.0;
+            x += 1.0;
+            gc.drawImage(new Image("asteroids/spaceship.png"), 100.0, this.x);
+            gc.rotate(1.0);
         }
-
-    };
-
-    loop.start();
-    
-    
+    };  
 }
+
+
